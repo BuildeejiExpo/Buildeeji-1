3e4f168db987f45582aef82c857d7d575dd17e98
chandud2020 <chandra.mca@gmail.com>
2016-02-14 01:31:35 -0600

    test.

--- Buildeeji/Global.asax.cs
+++ Buildeeji/Global.asax.cs
@@ -16,8 +16,6 @@ namespace Buildeeji
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-
         }
     }
 }
